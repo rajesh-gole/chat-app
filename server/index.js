22f166e7ae10a83ab6687c9672e7b07f16e8e4a5
RAJESH GOLE <rajeshgole96@gmail.com>
2023-10-31 16:15:11 +0530

    connecting to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,13 +1,23 @@
 const express = require("express")
 const cors = require("cors")
+const mongoose = require("mongoose")
 
 const app = express()
+require("dotenv").config()
 
 app.use(express.json())
 app.use(cors())
 
 const port = process.env.PORT || 5000;
+const uri = process.env.ATLUS_URI;
 
 app.listen(port, (req, res) => {
     console.log(`Server running on ${port}`);
 })
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log("MongoDB connected Successfully"))
+    .catch((error) => console.log("MongoDB connection failed : ", error.message))
