a139c355b7999a6c6bf06ddfa342f4d2764765d5
RAJESH GOLE <rajeshgole96@gmail.com>
2023-11-01 09:27:24 +0530

    Find single user

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -68,4 +68,18 @@ const loginUser = async (req, res) => {
     }
 }
 
-module.exports = {registerUser, loginUser}
+//Find single user
+
+const findUser = async (req, res) => {
+    const userId = req.params.userId;
+    try {
+        const user = await userModel.findById(userId) 
+        res.status(200).json(user)
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error)
+    }
+}
+
+module.exports = {registerUser, loginUser, findUser}
