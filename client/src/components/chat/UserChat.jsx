8482873d1c28ece8aa93c7813809bc5ccad902b1
RAJESH GOLE <rajeshgole96@gmail.com>
2023-11-01 18:05:55 +0530

    list chats

---
+++ client/src/components/chat/UserChat.jsx
@@ -0,0 +1,30 @@
+import { Stack } from "react-bootstrap";
+import { useFetchRecipientUser } from "../../hooks/userFetchRecipient";
+import avatar1 from "../../assets/avatar1.svg"
+
+const UserChat = ({chat, user}) => {
+    const { recipientUser } = useFetchRecipientUser(chat, user)
+    
+    return (<>
+        <Stack direction="horiontal" gap={3} className="user-card align-items-center p-2 justify-content-between">
+            <div className="d-flex">
+                <div className="me-2">
+                    <img src={avatar1} height="35px" />
+                </div>
+                <div className="text-content">
+                    <div className="name">{recipientUser?.name}</div>
+                    <div className="text">Text Message</div>
+                </div>
+            </div>
+            <div className="d-flex flex-column align-items-end">
+                <div className="date">
+                    01/11/2023
+                </div>
+                <div className="this-user-notifications">2</div>
+                <span className="user-online"></span>
+            </div>
+        </Stack>
+    </> );
+}
+ 
+export default UserChat;
