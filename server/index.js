c1408e971b8ea5402eb642c741cc5128e5ab687f
RAJESH GOLE <rajeshgole96@gmail.com>
2023-11-01 08:25:58 +0530

    update

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
+const userRoute = require("./Routes/userRoute")
 
 const app = express()
 require("dotenv").config()
 
 app.use(express.json())
 app.use(cors())
+app.use("/api/users", userRoute)
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Chat-App")
+})
 
 const port = process.env.PORT || 5000;
 const uri = process.env.ATLUS_URI;
