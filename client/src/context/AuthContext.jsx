8aca6489131e0d58b436a8228f67e46ebd5e2974
RAJESH GOLE <rajeshgole96@gmail.com>
2023-11-01 11:12:00 +0530

    context API

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useCallback, useState } from "react";
+
+export const AuthContext = createContext()
+
+export const AuthContextProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [registerInfo, setRegisterInfo] = useState({
+        name: "",
+        email: "",
+        password: "",
+    })
+
+    const updateRegisterInfo = useCallback((info) => {
+        setRegisterInfo(info)
+    }, [])
+    console.log(registerInfo);
+    return <AuthContext.Provider value={{user, registerInfo, updateRegisterInfo}}>
+        {children}
+    </AuthContext.Provider>
+}
