ca41e53a777521920c70874e3ecd634e469e02d7
RAJESH GOLE <rajeshgole96@gmail.com>
2023-11-01 09:12:36 +0530

    User login api

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -44,6 +44,28 @@ const registerUser = async (req, res) => {
         console.log(error);
         res.status(500).json(error)
     }
-    }
+}
 
-module.exports = {registerUser}
+//login user
+
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        let user = await userModel.findOne({ email });
+        if (!user)
+            return res.status(400).json("Invalid email or password!")
+
+        const isValidPassword = await bcrypt.compare(password, user.password)
+        if (!isValidPassword)
+            return res.status(400).json("Invalid email or password...!")
+        
+        const token = createToken(user._id);
+        res.status(200).json({_id: user._id, name : user.name, email, token})
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error)
+    }
+}
+
+module.exports = {registerUser, loginUser}
